dda5a179bb9c5f4905f65ee9651ee4d8f3112d5f
Carlos Junior <carloslaujunior23@gmail.com>
2026-08-27 17:28:21 -0300

    Conexao com banco MySQL funcionando - entidade User validada

--- backend/src/main/java/com/atpromo/systematpromo/model/User.java
+++ backend/src/main/java/com/atpromo/systematpromo/model/User.java
@@ -14,6 +14,8 @@ public class User {
     private String name;
     private String email;
     private String password;
+
+    @Column(name = "jobTittle")
     private String jobTittle;
 
     public User() {
